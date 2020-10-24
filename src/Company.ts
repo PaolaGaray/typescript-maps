301f31dc3158fd9a8e337a8e063010db2577977a
Paola Garay <garay.angela@gmail.com>
2020-10-24 12:26:58 +0200

    Add User and Company classes

--- src/Company.ts
+++ src/Company.ts
@@ -1,13 +1,19 @@
 import faker from 'faker';
 
-class Company {
-    name: string;
+export class Company {
+    companyName: string;
+    catchPhrase: string;
     location: {
         lat: number;
         lng: number;
     };
 
     constructor() {
-
+        this.companyName = faker.company.companyName();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            lat : parseFloat(faker.address.latitude()),
+            lng : parseFloat(faker.address.longitude())
+        }
     }
 }
